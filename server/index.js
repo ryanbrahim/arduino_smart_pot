1f0f21f684e7edcea7aea371b78958cadd9e2257
Ryan Almizyed <almizyed@wisc.edu>
2023-04-28 17:45:58 -0500

    Implement simple webhook to print out sensor data to console

--- server/index.js
+++ server/index.js
@@ -4,10 +4,25 @@ const bodyParser = require("body-parser")
 
 // Initialize express and define a port
 const app = express()
-const PORT = 3000
+const PORT = 25565
 
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.json())
 
 // Start express on the defined port
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+// Testing express server
+app.get('/', (req, res) => {
+  res.status(200).send(JSON.stringify({
+    msg: 'Express Server Works!'
+  }))
+})
+
+// Define webhook
+app.use(bodyParser.json())
+app.post("/hook", (req, res) => {
+  // console.log(`Got a POST: ${JSON.stringify(req.body, null, 2)}`) // Call your action on the request here
+  console.log(`Moisture level: ${req.body.data} at ${req.body.published_at}`)
+  res.status(200).end() // Responding is important
+})
